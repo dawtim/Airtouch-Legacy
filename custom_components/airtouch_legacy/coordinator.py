217b5cbc146800a0a5891f6c7a655b9a50a9be85
dawtim <104969451+dawtim@users.noreply.github.com>
2026-03-15 13:35:04 +0800

    Update coordinator.py

--- custom_components/airtouch_legacy/coordinator.py
+++ custom_components/airtouch_legacy/coordinator.py
@@ -1,17 +1,18 @@
 from datetime import timedelta
 import logging
+
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class AirTouchCoordinator(DataUpdateCoordinator):
     def __init__(self, hass, client) -> None:
-
-    super().__init__(
-        hass,
-        _LOGGER,
-        name="AirTouch",
-        update_interval=timedelta(seconds=10),
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="AirTouch",
+            update_interval=timedelta(seconds=10),
         )
         self.client = client
 
