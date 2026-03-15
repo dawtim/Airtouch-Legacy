d62b4a606640c9c0cbc6eebcd2198a8e4767d4b0
dawtim <104969451+dawtim@users.noreply.github.com>
2026-03-15 13:40:09 +0800

    Update config_flow.py

--- custom_components/airtouch_legacy/config_flow.py
+++ custom_components/airtouch_legacy/config_flow.py
@@ -1,6 +1,8 @@
 import voluptuous as vol
 from homeassistant import config_entries
+
 from .const import DOMAIN
+
 
 class AirTouchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -9,9 +11,16 @@ class AirTouchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             await self.async_set_unique_id(user_input["host"])
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=f"AirTouch {user_input['host']}", data=user_input)
+            return self.async_create_entry(
+                title=f"AirTouch {user_input['host']}",
+                data=user_input,
+            )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required("host"): str}),
+            data_schema=vol.Schema(
+                {
+                    vol.Required("host"): str,
+                }
+            ),
         )
